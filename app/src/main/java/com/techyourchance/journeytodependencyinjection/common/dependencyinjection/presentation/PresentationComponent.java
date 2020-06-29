b18003229d6c31eef4e450cbbea2b2fec14688c0
Rangga Bagas <rangga@aleph-labs.com>
2020-06-29 19:52:32 +0700

    PresentationComponent as Injector

--- app/src/main/java/com/techyourchance/journeytodependencyinjection/common/dependencyinjection/presentation/PresentationComponent.java
+++ app/src/main/java/com/techyourchance/journeytodependencyinjection/common/dependencyinjection/presentation/PresentationComponent.java
@@ -2,15 +2,24 @@ package com.techyourchance.journeytodependencyinjection.common.dependencyinjecti
 
 import com.techyourchance.journeytodependencyinjection.questions.FetchQuestionDetailsUseCase;
 import com.techyourchance.journeytodependencyinjection.questions.FetchQuestionsListUseCase;
+import com.techyourchance.journeytodependencyinjection.questions.Question;
 import com.techyourchance.journeytodependencyinjection.screens.common.dialogs.DialogsManager;
 import com.techyourchance.journeytodependencyinjection.screens.common.mvcviews.ViewMvcFactory;
+import com.techyourchance.journeytodependencyinjection.screens.questiondetails.QuestionDetailsActivity;
+import com.techyourchance.journeytodependencyinjection.screens.questionslist.QuestionsListActivity;
 
 import dagger.Component;
 
 @Component(modules = PresentationModule.class)
 public interface PresentationComponent {
-    DialogsManager getDialogsManager();
-    ViewMvcFactory getViewMvcFactory();
-    FetchQuestionsListUseCase getFetchQuestionsListUseCase();
-    FetchQuestionDetailsUseCase getFetchQuestionDetailsUseCase();
+//    DialogsManager getDialogsManager();
+//    ViewMvcFactory getViewMvcFactory();
+//    FetchQuestionsListUseCase getFetchQuestionsListUseCase();
+//    FetchQuestionDetailsUseCase getFetchQuestionDetailsUseCase();
+
+    // injection method to a client specified in param
+    void inject(QuestionsListActivity activity);
+
+    // the name of "inject" is not a must. Below is the example of random name 
+    void blabla(QuestionDetailsActivity activity);
 }
--- app/src/main/java/com/techyourchance/journeytodependencyinjection/screens/common/activities/BaseActivity.java
+++ app/src/main/java/com/techyourchance/journeytodependencyinjection/screens/common/activities/BaseActivity.java
@@ -4,9 +4,6 @@ import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
 
 import com.techyourchance.journeytodependencyinjection.MyApplication;
-import com.techyourchance.journeytodependencyinjection.common.dependencyinjection.CompositionRoot;
-import com.techyourchance.journeytodependencyinjection.common.dependencyinjection.Injector;
-import com.techyourchance.journeytodependencyinjection.common.dependencyinjection.PresentationCompositionRoot;
 import com.techyourchance.journeytodependencyinjection.common.dependencyinjection.application.ApplicationComponent;
 import com.techyourchance.journeytodependencyinjection.common.dependencyinjection.presentation.DaggerPresentationComponent;
 import com.techyourchance.journeytodependencyinjection.common.dependencyinjection.presentation.PresentationComponent;
@@ -17,30 +14,17 @@ public class BaseActivity extends AppCompatActivity {
     private boolean mIsInjectorUsed;
 
     @UiThread
-    protected Injector getInjector() {
+    protected PresentationComponent getPresentationComponent() {
         if (mIsInjectorUsed) {
             throw new RuntimeException("there is no need to use injector more than once");
         }
         mIsInjectorUsed = true;
-        //return new Injector(getCompositionRoot());
-        return new Injector(getPresentationComponent());
-    }
-
-    private PresentationComponent getPresentationComponent() {
         return DaggerPresentationComponent.builder()
                 .presentationModule(new PresentationModule(this, getApplicationComponent()))
                 .build();
     }
-//    private PresentationCompositionRoot getCompositionRoot() {
-//        //return new PresentationCompositionRoot(getAppCompositionRoot(),this);
-//        return new PresentationCompositionRoot(getApplicationComponent(), this);
-//    }
 
     private ApplicationComponent getApplicationComponent() {
         return ((MyApplication) getApplication()).getApplicationComponent();
     }
-
-//    private CompositionRoot getAppCompositionRoot() {
-//        return ((MyApplication) getApplication()).getCompositionRoot();
-//    }
 }
